5661f2e151fafa79171250111066588999b0c8f8
Serena <Sere.Brow@Outlook.com>
2024-04-06 21:06:41 -0400

    updating credential options

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,5 @@
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+// import GitHubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/app/(models)/User";
 import bcrypt from "bcrypt";
@@ -8,37 +8,37 @@ import bcrypt from "bcrypt";
 
 export const options = {
   providers: [
-    GitHubProvider({
-      profile(profile) {
-        console.log("GitHub Profile: ", profile);
-        let userRole = "GitHub User";
-        if (profile?.email == "sere.brow@outlook.com") {
-          userRole = "admin";
-        }
-        return {
-          ...profile,
-          role: userRole,
-        };
-      },
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      profile(profile) {
-        console.log("Google Profile: ", profile);
-        let userRole = "Google User";
-        if (profile?.email == "meowpoops@gmail.com") {
-          userRole = "admin";
-        }
-        return {
-          ...profile,
-          id: profile.sub,
-          role: userRole,
-        };
-      },
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    // GitHubProvider({
+    //   profile(profile) {
+    //     console.log("GitHub Profile: ", profile);
+    //     let userRole = "GitHub User";
+    //     // if (profile?.email == "admin@gastroluxe.com") {
+    //     //   userRole = "admin";
+    //     // }
+    //     return {
+    //       ...profile,
+    //       role: userRole,
+    //     };
+    //   },
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    // GoogleProvider({
+    //   profile(profile) {
+    //     console.log("Google Profile: ", profile);
+    //     let userRole = "Google User";
+    //     // if (profile?.email == "admin@gastroluxe.com") {
+    //     //   userRole = "admin";
+    //     // }
+    //     return {
+    //       ...profile,
+    //       id: profile.sub,
+    //       role: userRole,
+    //     };
+    //   },
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
